60a967804d9a1f4e1880c2a8f375b6b642731e85
zhangmengyu10 <742377801@qq.com>
2019-07-10 09:28:08 +0800

    克隆后对MyFirstDemo增加注释

--- test_git_03/src/com/aaa/MyFirstDemo.java
+++ test_git_03/src/com/aaa/MyFirstDemo.java
@@ -3,11 +3,14 @@ package com.aaa;
 public class MyFirstDemo {
     public static void main(String[] args) {
         System.out.println("git小测试");
+        //这是新增的注释
     }
 
     public void test(){
         int i  = 0 ;
         i += 10;
+        System.out.println(i);
+        System.out.println(i);
         System.out.println(i);
     }
 }
