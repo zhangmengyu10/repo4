c573afdca3875da4a3b0a55aac75cf7ac8c8e93d
zhangmengyu10 <742377801@qq.com>
2019-07-10 09:31:05 +0800

    克隆后第二次对MyFirstDemo修改，调用了test方法

--- test_git_03/src/com/aaa/MyFirstDemo.java
+++ test_git_03/src/com/aaa/MyFirstDemo.java
@@ -4,9 +4,10 @@ public class MyFirstDemo {
     public static void main(String[] args) {
         System.out.println("git小测试");
         //这是新增的注释
+        test();
     }
 
-    public void test(){
+    public static void test(){
         int i  = 0 ;
         i += 10;
         System.out.println(i);
