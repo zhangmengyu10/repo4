a262650e5eba63dd56d0dcf4ec5082f13f2f2315
zhangmengyu10 <742377801@qq.com>
2019-07-10 08:58:15 +0800

    修改类MyFirstDemo，添加了一个test方法

--- test_git_03/src/com/aaa/MyFirstDemo.java
+++ test_git_03/src/com/aaa/MyFirstDemo.java
@@ -4,4 +4,10 @@ public class MyFirstDemo {
     public static void main(String[] args) {
         System.out.println("git小测试");
     }
+
+    public void test(){
+        int i  = 0 ;
+        i += 10;
+        System.out.println(i);
+    }
 }
